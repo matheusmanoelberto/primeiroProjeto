81ea80dc0f50f24fa9b8e0592ea661694fe2a98e
matheusmanoelberto <matheusmanoel51@gmail.com>
2023-06-16 23:24:56 -0300

    Initial commit

---
+++ primeiroProjeto/Program.cs
@@ -0,0 +1,28 @@
+﻿//Screen  Sound
+
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+
+void ExibirMensagemDeBoasVidas()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");//Verbatim Literal
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registra uma banda");
+    Console.WriteLine("Digite 2 para mostra todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digide 4 para exibir a média de uma banda");
+    Console.WriteLine("digite 0 para sair");
+}
+
+ExibirMensagemDeBoasVidas();
+ExibirOpcoesDoMenu();
